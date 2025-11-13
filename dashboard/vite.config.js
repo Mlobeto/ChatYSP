92f7c07bd451390f340e106327fcafd9386981d7
mlobeto <mercedeslobeto@gmail.com>
2025-11-13 15:12:12 -0300

    verlogo

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -12,6 +12,12 @@ export default defineConfig({
   },
   server: {
     port: 3001,
+    host: true,
+    strictPort: true,
+    open: false,
+    hmr: {
+      overlay: true,
+    },
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
@@ -20,8 +26,19 @@ export default defineConfig({
       },
     },
   },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom', '@reduxjs/toolkit'],
+  },
   build: {
     outDir: 'dist',
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          redux: ['@reduxjs/toolkit', 'react-redux'],
+        },
+      },
+    },
   },
 })
